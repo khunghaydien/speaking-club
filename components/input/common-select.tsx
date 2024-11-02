9f890ca58df6ca6a4eaf341b9197c93be5b7d97c
Dat_PH <phungdat0902@gmail.com>
2024-11-02 19:42:48 +0700

    change select box type

--- components/input/common-select.tsx
+++ components/input/common-select.tsx
@@ -3,30 +3,43 @@ import {
   Autocomplete,
   AutocompleteProps,
   AutocompleteRenderInputParams,
+  ChipTypeMap,
   CircularProgress,
 } from "@mui/material";
 import CommonInput from "./common-input";
 import { ArrowDropDown } from "@mui/icons-material";
 
-export type OptionProps = {
-  value: string | number;
-  label: string;
-  image?: string;
-  icon?: string;
-};
-
-type CommonSelectProps<T> = Omit<
-  AutocompleteProps<OptionProps, false, false, false>,
-  "renderInput"
-> & {
+export interface CustomAutocompleteProps<
+  Value,
+  Multiple extends boolean | undefined,
+  DisableClearable extends boolean | undefined,
+  FreeSolo extends boolean | undefined,
+  ChipComponent extends React.ElementType = ChipTypeMap["defaultComponent"]
+> extends Omit<
+    AutocompleteProps<
+      Value,
+      Multiple,
+      DisableClearable,
+      FreeSolo,
+      ChipComponent
+    >,
+    "renderInput"
+  > {
   renderInput?: (params: AutocompleteRenderInputParams) => ReactNode;
   label?: React.ReactNode;
   error?: boolean;
   helperText?: React.ReactNode;
   onScrollTop?: () => void;
-};
+}
 
-function CommonSelect<T>({
+function CommonSelect<
+  Value,
+  Multiple extends boolean | undefined,
+  DisableClearable extends boolean | undefined,
+  FreeSolo extends boolean | undefined,
+  ChipComponent extends React.ElementType = ChipTypeMap["defaultComponent"]
+>({
+  multiple,
   label,
   error,
   helperText,
@@ -37,7 +50,13 @@ function CommonSelect<T>({
   renderInput,
   popupIcon = <ArrowDropDown />,
   ...props
-}: CommonSelectProps<T>) {
+}: CustomAutocompleteProps<
+  Value,
+  Multiple,
+  DisableClearable,
+  FreeSolo,
+  ChipComponent
+>) {
   const handleScroll = (event: React.UIEvent<HTMLUListElement>) => {
     const listboxNode = event.currentTarget;
     if (
@@ -50,6 +69,7 @@ function CommonSelect<T>({
   return (
     <Autocomplete
       {...props}
+      multiple={multiple}
       fullWidth
       loading={loading}
       autoHighlight
